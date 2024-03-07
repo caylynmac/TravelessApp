5b8d31723b4ead81959ed26d7771e6451976fcb8
TortoiseGhos <152567038+TortoiseGhos@users.noreply.github.com>
2024-03-07 11:23:58 -0700

    Update SearchReservationsPage.xaml.cs

--- Views/SearchReservationsPage.xaml.cs
+++ Views/SearchReservationsPage.xaml.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TravelessApp.Models;
+
 namespace TravelessApp.Views;
 
 public partial class SearchReservationsPage : ContentPage
@@ -5,5 +11,28 @@ public partial class SearchReservationsPage : ContentPage
 	public SearchReservationsPage()
 	{
 		InitializeComponent();
-	}
+    	}
+
+
+    private void FindReservation_Clicked(object sender, EventArgs e)
+    	{
+
+        string code = FlightCode.Text;
+        string airline = Airline.Text;
+        string name = Name.Text;
+
+
+        // Find flights with specified parameters
+        List<Flight> foundReservations = FlightsRepository.FindReservations(code, airline, name);
+
+        if (foundReservations != null && foundReservations.Any())
+        {
+            reservationPicker.ItemsSource = foundReservations;
+        }
+        else
+        {
+            DisplayAlert("No Flights Found", "No flights found for the selected criteria.", "OK");
+        }
+    	}
+
 }
